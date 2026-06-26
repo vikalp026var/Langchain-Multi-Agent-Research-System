0d921c5ee56a5e804bfdbfcbbed7468c242827de
Vikalp <vikalp026varshney@gmail.com>
2026-06-26 18:05:12 +0530

    add tools

---
+++ main.py
@@ -0,0 +1,7 @@
+from tools.tools import web_search, scrape_url
+
+# output = web_search("WHat is the latest news on AI?")
+# results = scrape_url.invoke({"url": "https://www.artificialintelligence-news.com"})
+# print(results)\
+
+print(web_search.invoke('What is the latest research on AI'))
