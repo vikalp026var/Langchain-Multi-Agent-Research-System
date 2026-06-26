fb64a4f7fc06318b5756087bdbf34017936d6975
Vikalp <vikalp026varshney@gmail.com>
2026-06-26 18:48:06 +0530

    add multi agent

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from tools.tools import web_search, scrape_url
+from pipeline.pipeline import research_pipeline
 
 # output = web_search("WHat is the latest news on AI?")
 # results = scrape_url.invoke({"url": "https://www.artificialintelligence-news.com"})
 # print(results)\
 
-print(web_search.invoke('What is the latest research on AI'))
+print(research_pipeline('What is the effect on  Job due to AI'))
